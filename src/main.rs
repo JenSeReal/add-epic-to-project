e966b5e6660e0f6c7183398f586512399b543aa0
Jens Plüddemann <jens.plueddmann@novatec-gmbh.de>
2023-03-03 09:04:26 +0100

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 use std::env;
-use std::fs::write;
-use std::process::exit;
+// use std::fs::write;
+// use std::process::exit;
 
 fn main() {
-  let github_output_path = env::var("GITHUB_OUTPUT").unwrap();
+  // let github_output_path = env::var("GITHUB_OUTPUT").unwrap();
 
   let args: Vec<String> = env::args().collect();
 
   println!("{:#?}", args);
 
-  let error = &args[1];
+  // let error = &args[1];
 
-  if !error.is_empty() {
-    eprintln!("Error: {error}");
-    write(github_output_path, format!("error={error}")).unwrap();
-    exit(1);
-  }
+  // if !error.is_empty() {
+  //   eprintln!("Error: {error}");
+  //   write(github_output_path, format!("error={error}")).unwrap();
+  //   exit(1);
+  // }
 }
