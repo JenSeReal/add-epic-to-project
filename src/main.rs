8261b6520dfbc5a813b286c41b2f917d1de87790
Jens Plüddemann <jens.plueddmann@novatec-gmbh.de>
2023-03-06 14:12:29 +0100

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use std::{env, fs};
 
-use octocrab::{etag::Etagged, models::IssueEvent, Page};
-
 use crate::models::{Args, Params};
 // use std::fs::write;
 // use std::process::exit;
@@ -13,20 +11,15 @@ mod models;
 async fn main() -> anyhow::Result<(), anyhow::Error> {
   // let mut github_output_path = env::var("GITHUB_OUTPUT").unwrap();
 
-  let args = Args(env::args().collect());
-  dbg!(args.clone());
-
-  let params = Params::try_from(args)?;
-
-  dbg!(params.clone());
+  let params = Params::try_from(Args(env::args().collect()))?;
 
   let crab = octocrab::OctocrabBuilder::new()
     .personal_token(params.github_token().to_string())
     .build()?;
 
-  dbg!(crab.clone());
-
   let event = fs::read_to_string(env::var("GITHUB_EVENT_PATH")?)?;
+
+  dbg!(&event);
 
   // let event_crab: IssueEvent = crab.events().send().await?;
 
